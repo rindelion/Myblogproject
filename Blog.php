f3a5051544051da2ee2f485c79cb1527b4a7ca3e
Rin <72259594+rindelion@users.noreply.github.com>
2021-07-02 16:30:37 +0700

    Update Blog.php

--- Blog.php
+++ Blog.php
@@ -58,24 +58,21 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="Blog.php">Simple Blog</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <a href="about.html">About</a>
-            </li>
-            <li>
-              <a href="login.html">Profile</a>
-            </li>
-            <li>
-              <a href="index.php">Log out</a>
-            </li>
-            <!-- <li>
-              <a href="signup.html">Sign up</a>
-            </li> -->
-          </ul>
-        </div>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                <a href="about.html">About</a>
+              </li>
+              <li> 
+                  <a href="profile.php">Profile</a>
+              </li>
+              <li>
+                <a href="index.php">Logout</a>
+              </li>
+            </ul>
+          </div>
         <!-- /.navbar-collapse -->
       </div>
       <!-- /.container -->
